03ff7aefc8089d6c40bee5c60aa1adeec223f975
Arjun Dubey <40758425+arjundubey-cr@users.noreply.github.com>
2019-01-26 18:07:02 +0530

    Create mario_more_comfortable.c

---
+++ Pset1/mario_more_comfortable.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    int x;
+    do
+        {
+            x = get_int("height of pyramid: ");
+        }
+    while (x<0||x>23);
+    for (int i=0; i<x; i++)
+    {
+        for ()
+    }
+
+printf ("%d \n", x);
+}
